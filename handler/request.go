5352bdd8b17d6504f3cbb2e78f58ff3ee7d4c058
llyas36 <eliyasinbox1@gmail.com>
2025-11-29 20:20:41 +0300

    rename funcs/vars in request handler

--- handler/request.go
+++ handler/request.go
@@ -1,9 +1,10 @@
 package httphandler
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	
+	"xo/internal"
 )
 
 func CreateGetRequest(url string) (*http.Request, error) {
@@ -17,7 +18,7 @@ func CreateGetRequest(url string) (*http.Request, error) {
 }
 
 func setupHeader(httpReqeust *http.Request) {
-	httpReqeust.Header.Set("Authorization", "Bearer "+config.BearerToken)
+	httpReqeust.Header.Set("Authorization", "Bearer "+internal.BearerToken)
 
 }
 
@@ -33,25 +34,25 @@ func GetResponse(httpRequest *http.Request) (*http.Response, error) {
 func UsersLookup(usernames string, user_fields string) (*http.Request, error) {
 	//usernames := "elonmusk,llyas__"
 	//baseURL := "https://api.x.com/2/users/by"
-	subURL := "/users/by/username/"
+//	subURL := "/users/by/username/"
 	// user_fields := "user.fields=description,created_at" // optional
 
 	// params := url.Values{}
 	// params.Add("usernames", usernames)
 	// params.Add("user.Fields", user_fields)
 
-	//fullURL := fmt.Sprintf("%s?%s", baseURL, params.Encode())
-	fullURL := config.CreateURL(usernames, user_fields, subURL)
+//	fullURL := fmt.Sprintf("%s?%s", internal.BaseURL, params.Encode())
+		
 	// data(username)
 	// fields(user_fields)
 	// subURL(/users/by/username/{%s}
 	// parmas(&parmas)
-	httpRequest, err := CreateGetRequest(fullURL)
-	if err != nil {
-		return httpRequest, err
-	}
+//	httpRequest, err := CreateGetRequest(fullURL)
+//	if err != nil {
+//		return httpRequest, err
+//	}
 
-	return httpRequest, nil
+	return nil, nil
 
 }
 
@@ -61,7 +62,8 @@ func GetTweetsById(ids string, tweet_fields string) *http.Response {
 	// fields(tweet_field)
 	// subURL
 	//
-	url := config.CreateURL("", "", subURL)
+	
+	url := fmt.Sprintf("%s/%s", internal.BaseURL, subURL)
 	// bearer_token := config.ReturnEnv()
 	req, err := CreateGetRequest(url)
 	if err != nil {
